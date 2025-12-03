bb1528a1d86f1c1afdf4f55a3299cf0cda1edafd
Alex Thanas <greek313@gmail.com>
2025-12-03 16:41:46 -0600

    Added eslint, no errors per lint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const Note = require('./models/notes')
-const app = express();
+const app = express()
 
 
 const requestLogger = (request, response, next) => {
-    console.log('Method', request.method);
-    console.log('Path', request.path);
-    console.log('Body', request.body);
-    console.log('---');
-    next()
+  console.log('Method', request.method)
+  console.log('Path', request.path)
+  console.log('Body', request.body)
+  console.log('---')
+  next()
 }
 
 // This allows us to use the json parser in express lower in our code
@@ -22,26 +22,26 @@ app.use(express.static('dist'))
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
+  response.send('<h1>Hello World!</h1>')
 })
 // GET all notes
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes);
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 //GET specific note from mongo DB
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end();
-            }
-        })
-        .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // GET specific note conveting id and looking up through notes array
@@ -57,18 +57,13 @@ app.get('/api/notes/:id', (request, response, next) => {
 // })
 // delete
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end();
-        })
-        .catch(error => next(error))
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 //depreciated id generator
-const generateId = () => {
-    //easy max id, Remember "...notes" is taking a copy of the notes array
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-    return String(maxId + 1)
-}
 // Post request by creating object and then concatin to array
 
 // app.post('/api/notes', (request,response) => {
@@ -88,66 +83,66 @@ const generateId = () => {
 //  })
 //post request mongo DB
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
 
-    note.save().then(savedNote => {
-        response.json(savedNote);
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
     .catch(error => next(error))
 })
 
 //PUT to update notes imprortance
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
+  const { content, important } = request.body
 
-    Note.findById(request.params.id)
-        .then(note => {
-            if (!note) {
-                return response.status(404).end();
-            }
-        
-        note.content = content
-        note.important = important
-        
-        return note.save().then(updatedNote => {
-            response.json(updatedNote);
-        })    
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'Unkown Endpoint' })
+  response.status(404).send({ error: 'Unkown Endpoint' })
 }
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 
 // const app = http.createServer((request, response) => {
@@ -156,5 +151,5 @@ app.use(errorHandler);
 // })
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`)
 })
--- models/notes.js
+++ models/notes.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
-mongoose.connect(url, {family: 4}).then(result => {
-  console.log('connected to MongoDB');
+mongoose.connect(url, { family: 4 }).then(() => {
+  console.log('connected to MongoDB')
 }).catch(error => {
-    console.log('error connecting to MongoDB', error.message);
+  console.log('error connecting to MongoDB', error.message)
 })
 
 const noteSchema = new mongoose.Schema({
-    // can also add validation here from mongoose
-    content: {
-        type: String,
-        minlength: 5,
-        required: true
-    },
-    important: Boolean
+  // can also add validation here from mongoose
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  important: Boolean
 })
 // Transforming the returned object to modify _id to id and removing __v
 noteSchema.set('toJSON' , {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
