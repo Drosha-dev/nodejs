e7ccbddda42dfc8a8e25e3036b77ed65c7ae4de4
Alex Thanas <greek313@gmail.com>
2025-12-29 17:26:36 -0600

    Added users id to notes, users now have notes and populate them into a user, also notes have Users that post them and populate their details in notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -9,7 +9,7 @@ const User = require('../models/user')
 // })
 //async get
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username:1, name:1 })
   response.json(notes)
 })
 
@@ -25,15 +25,23 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if(!user){
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
-
 // notesRouter.delete('/:id', (request, response, next) => {
 //   Note.findByIdAndDelete(request.params.id)
 //     .then(() => {
